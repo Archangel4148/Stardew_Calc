69098f7a070502189d4c3314e2808e1012e61d67
Simon Edmunds <sjedmunds04@gmail.com>
2024-09-30 14:27:33 -0500

    Added base framework for table management

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
 from ui.main_window_init import Ui_main_window
 from PyQt5 import QtWidgets
+
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
@@ -10,6 +13,24 @@ class MainWindow(QtWidgets.QWidget):
         # Initialize UI
         self.ui = Ui_main_window()
         self.ui.setupUi(self)
+
+        # Table model
+        self.model = QStandardItemModel()
+        self.ui.crop_table_view.setModel(self.model)
+
+        self.populate_table()
+
+    def populate_table(self):
+        row_count, column_count = 10, 3
+
+        self.model.setHorizontalHeaderLabels([f"Header {i+1}" for i in range(column_count)])
+
+        for i in range(row_count):
+            self.add_row([j * j for j in range(column_count)])
+
+    def add_row(self, data: list):
+        row_data = [QStandardItem(str(val)) for val in data]
+        self.model.appendRow(row_data)
 
 
 if __name__ == '__main__':
