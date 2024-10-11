acc52783a3cef5f8492d95e09d99d809167f8dbc
Simon Edmunds <sjedmunds04@gmail.com>
2024-10-11 11:04:44 -0500

    Implemented the Crop class to store the received crop data

--- crops.py
+++ crops.py
@@ -1,9 +1,24 @@
+import dataclasses
+
 from bs4 import BeautifulSoup
 
 from data_fetcher import DataFetcher
 
 
-def get_crops():
+@dataclasses.dataclass(kw_only=True)
+class Crop:
+    name: str
+    seed_name: str
+    edible: bool
+    multiple_rarities: bool
+    used_in_recipe: bool
+    purchase_sources: dict[str:int]
+    sell_prices: list[int] | None
+    energy_values_by_rarity: list[int] | None
+    health_values_by_rarity: list[int] | None
+
+
+def get_crops() -> list[Crop]:
     data_fetcher = DataFetcher("https://stardewvalleywiki.com/Crops")
 
     raw_data: BeautifulSoup = data_fetcher.raw_data
@@ -11,7 +26,7 @@ def get_crops():
     # Find all tables on the page
     tables = raw_data.find_all('table', class_='wikitable')
 
-    crops = []
+    crops: list[Crop] = []
 
     for table in tables:
 
@@ -31,23 +46,37 @@ def get_crops():
         multiple_rarities = len(clean_data[0]) == 16 if is_edible else len(clean_data[0]) == 7
         usable = "used in" in [header.casefold() for header in headers]
         seed_name = clean_data[0][0]
-        purchase_source = clean_data[0][1]
-        sell_prices = clean_data[0][2:6] if multiple_rarities else [clean_data[0][2]]
-        if is_edible:
-            # edible + rare = 16
-            if usable:
-                energy_values = clean_data[0][-9:-1:2]
-                health_values = clean_data[0][-8:-1:2]
-            else:
-                energy_values = clean_data[0][-8:-1:2]
-                health_values = clean_data[0][-7:len(clean_data[0]):2]
-        else:
-            # inedible + rare = 7
-            energy_values = "None"
-            health_values = "None"
+        purchase_data = clean_data[0][1].strip().split(" ")
+        purchase_sources = {purchase_data[i][:-1]: int(purchase_data[i + 1].split(">")[-1][:-1]) for i in
+                            range(0, len(purchase_data), 2)}
 
-        # print("========\n", crop_name, f"\nEdible: {is_edible}\n", clean_data)
-        print(f"=============\n{crop_name}\nSeed: {seed_name}\nEdible: {is_edible}\nMultiple Rarities: {multiple_rarities}\nUsable: {usable}\nPurchase Source: {purchase_source}\nSell Prices: {sell_prices}\nEnergy Values: {energy_values}\nHealth Values: {health_values}")
+        sell_prices = clean_data[0][2:6] if multiple_rarities else [clean_data[0][2]]
+        sell_prices = [int(price.replace("g", "")) for price in sell_prices]
+        if is_edible:
+            if usable:
+                energy_values = list(map(int, clean_data[0][-9:-1:2]))
+                health_values = list(map(int, clean_data[0][-8:-1:2]))
+            else:
+                energy_values = list(map(int, clean_data[0][-8:-1:2]))
+                health_values = list(map(int, clean_data[0][-7:len(clean_data[0]):2]))
+        else:
+            energy_values = None
+            health_values = None
+
+        crops.append(
+            Crop(
+                name=crop_name,
+                seed_name=seed_name,
+                edible=is_edible,
+                multiple_rarities=multiple_rarities,
+                used_in_recipe=usable,
+                purchase_sources=purchase_sources,
+                sell_prices=sell_prices,
+                energy_values_by_rarity=energy_values,
+                health_values_by_rarity=health_values,
+            )
+        )
+        return crops
 
 
 if __name__ == '__main__':
